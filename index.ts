1cf8cdae7f70c9afebe680b7613e0846582f0770
Phips Peter <pspeter333@gmail.com>
2015-02-06 14:03:06 -0800

    Rearranging the register module
    This will read the configuration from the environment per file which
    will hopefully give developers more hooks to set the compilation
    settings as well as make it easier to test.

--- index.ts
+++ index.ts
@@ -4,51 +4,50 @@ import fs = require("fs");
 import path = require("path");
 import typescript = require("typescript");
 
-interface Module {
-    id: string;
-    exports: any;
-    parent: Module;
-    filename: string;
-    loaded: boolean;
-    children: Module[];
-    paths: string[];
-    _compile: (src: string, filename: string) => void;
+/**
+ * Requires a TypeScript file
+ * @param {Module} module   The node module
+ * @param {string} filename The module filename
+ */
+function req(module: Module, filename: string): void {
+    var options = compilerOptions();
+    var out = dest(filename, options);
+    if (!useCache() || isModified(filename, out)) {
+        compile(filename, options);
+    }
+    module._compile(fs.readFileSync(out, "utf8"), filename);
+}
+
+require.extensions[".ts"] = req;
+
+/**
+ * Checks the environment for whether or not we should emit an error. Defaults
+ * to true.
+ * @return {boolean} Whether or not to emit TypeScript errors
+ */
+function emitError(): boolean {
+    return process.env.TYPESCRIPT_REGISTER_EMIT_ERROR || true;
 }
 
 /**
- * The compiler options
- * @type {typescript.CompilerOptions}
+ * Checks the environment for whether or not we should use a build cache.
+ * Defaults to true.
+ * @return {boolean} Whether or not to use cached JavaScript files
  */
-var options: typescript.CompilerOptions = {
-    module: typescript.ModuleKind.CommonJS,
-    outDir: path.join(path.sep, "tmp", "tsreq"),
-    target: typescript.ScriptTarget.ES5
-};
-
-/**
- * The compiler host
- * @type {typescript.CompilerHost}
- */
-var host = typescript.createCompilerHost(options);
-
-/**
- * Initializes the compiler host
- * @param {typescript.CompilerOptions} opts The compiler options
- */
-function init(opts: typescript.CompilerOptions): void {
-    options = opts;
-    host = typescript.createCompilerHost(options);
+function useCache(): boolean {
+    return process.env.TYPESCRIPT_REGISTER_USE_CACHE || true;
 }
 
 /**
  * Returns the JavaScript destination for the path
- * @param  {string} filename The TypeScript filename
- * @return {string}          The JavaScript filepath
+ * @param  {string}                      filename The TypeScript filename
+ * @param  {typescript.CompilerOptiones} options  The Compiler Options
+ * @return {string}                               The JavaScript filepath
  */
-function dest(filename: string): string {
+function dest(filename: string, options: typescript.CompilerOptions): string {
+    var relativePath = path.relative(process.cwd(), filename);
     var outDir = options.outDir || process.cwd();
-    var basename = path.basename(filename, ".ts");
-    return path.join(outDir, basename + ".js");
+    return path.join(outDir, relativePath.replace(".ts", ".js"));
 }
 
 /**
@@ -68,27 +67,65 @@ function isModified(tsPath: string, jsPath: string): boolean {
 }
 
 /**
- * Requires a TypeScript file
- * @param {Module} m        The node module
- * @param {string} filename The module filename
+ * Compiles the TypeScript file
+ * @param {string}                      filename The root file to compile
+ * @param {typescript.CompilerOptiones} options  The Compiler Options
  */
-function req(module: Module, filename: string): void {
-    var out = dest(filename);
-    if (isModified(filename, out)) {
-        var program = typescript.createProgram([filename], options, host);
-        var source = program.getSourceFile(filename);
-        var checker = program.getTypeChecker(true);
-        var errors = checker.getDiagnostics();
-        if (errors.length > 0) {
-            throw new Error(errors.map((err: typescript.Diagnostic) => {
-                return err.messageText;
-            }).join("\n"));
-        }
-        checker.emitFiles(source);
+function compile(filename: string, options: typescript.CompilerOptions): void {
+    var host = typescript.createCompilerHost(options);
+    var program = typescript.createProgram([filename], options, host);
+    var source = program.getSourceFile(filename);
+    var checker = program.getTypeChecker(true);
+    if (emitError()) {
+        checkErrors(checker.getDiagnostics());
     }
-    module._compile(fs.readFileSync(out, "utf8"), filename);
+    checker.emitFiles(source);
 }
 
-require.extensions[".ts"] = req;
+/**
+ * Checks the environment for JSON stringified compiler options. By default it
+ * will compile TypeScript files into a scoped temp directory using ES5 and
+ * CommonJS targets.
+ * @return {typescript.CompilerOptions} The TypeScript compiler settings
+ */
+function compilerOptions(): typescript.CompilerOptions {
+    var env: string = process.env.TYPESCRIPT_REGISTER_COMPILER_OPTIONS;
+    if (env) {
+        return <typescript.CompilerOptions>JSON.parse(env);
+    }
+    return {
+        module: typescript.ModuleKind.CommonJS,
+        outDir: path.join(path.sep, "tmp", "tsreq", process.cwd()),
+        target: typescript.ScriptTarget.ES5
+    };
+}
 
-export = init;
+
+/**
+ * Converts a list of errors into something readable
+ * @param  {typescript.Diagnostic[]} errors The TypeScript Diagnostics
+ * @return {Error}                          The Compiler Error
+ */
+function checkErrors(errors: typescript.Diagnostic[]): void {
+    if (errors.length === 0) {
+        return;
+    }
+    var errorMessages = errors.map((err: typescript.Diagnostic): string => {
+        return err.messageText;
+    });
+    throw new Error(errorMessages.join("\n\n"));
+}
+
+/**
+ * The Module passed in by Node
+ */
+interface Module {
+    id: string;
+    exports: any;
+    parent: Module;
+    filename: string;
+    loaded: boolean;
+    children: Module[];
+    paths: string[];
+    _compile: (src: string, filename: string) => void;
+}
